dfbfd3cc8c8ae62cf54d3e000b88b0a483caf0c7
Ghostofelemantary@gmail.com <Ghostofelemantary@gmail.com@66844e33-a702-aea1-567c-434b7c635cab>
2013-11-24 00:39:45 +0000

    - Ajout d'une page post d'ajout  d'objet - Redirection dans la page controleur correspondante

--- controleurs/post_objet.php
+++ controleurs/post_objet.php
@@ -1,5 +1,5 @@
 <?php
-				require('modeles/Objets.php');
+				require('modeles/Objet.php');
 				
 				//On rename le fichier avec un nom hashé en md5 (histoire d'éviter les noms de fichiers foireux ...)
 				//Le fichier a été renommé de manière identique lors de son upload en AJAX
@@ -9,6 +9,8 @@
 				$id_image=md5($file_name);
 				$link_thumbnail = "imgs/objets/{$id_image}_thumbnail{$extension_upload}";
 				
-				$nouvel_objet = new Objets($_POST['nom_objet'], $_SESSION['login_entreprise'],$_POST['categorie_objet'],$_POST['description'],$link_thumbnail);
+				$nouvel_objet = new Objet($_POST['nom_objet'], $_SESSION['login_entreprise'],$_POST['categorie_objet'],$_POST['description'],$link_thumbnail);
 				$nouvel_objet->save();
+				
+				header( 'Location: http://projet.ulike.free.fr/post_ajout_objet.php');
 ?>
