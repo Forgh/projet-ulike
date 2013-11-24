47d67beff3f603a1113d111d02db7037cc89368a
Ghostofelemantary@gmail.com <Ghostofelemantary@gmail.com@66844e33-a702-aea1-567c-434b7c635cab>
2013-11-24 16:19:53 +0000

    - Correction d'une erreur de syntaxe php dans search.php - Ajout d'un message si la recherche d'objets ne retourne rien

--- controleurs/search.php
+++ controleurs/search.php
@@ -1,29 +1,32 @@
 <?php 
- $search = $GET['search'];
+ $search = $_GET['search'];
  require_once('modeles/Objet.php');
  
  $results=Objet::getLikeObjet($search);
- 
- while($carac_objet = $results->fetch()){
- ?>	
- 
- <div class="liste_objet_pair">	
-			
-		<img class="liste_objet_picture" src="<?php echo $carac_objet['img_objet']; ?>"  width="64px" height="64px"/>
-			
- 		<div class="liste_objet_info">
-				
-			<p><span class="presentationObjetLbl">Nom:</span><span class="presentationObjetNom"><?php echo $carac_objet['nom_proprietaire']; ?> - <?php echo $carac_objet['nom_objet']; ?></span></p>
-			<p><span class="presentationObjetLbl">Categorie:</span><?php echo $carac_objet['categorie_objet']; ?></p>
-			<p><span class="presentationObjetLbl">Description:</span><?php echo $carac_objet['description_objet']; ?></p>
+	if($results->rowCount() > 0){
+		 while($carac_objet = $results->fetch()){
+		 ?>	
+		 
+		 <div class="liste_objet_pair">	
 					
-		</div>
-			
-	</div>
-
-		<div class="bodycentered_line"></div>
-		<?php
- }
-
+				<img class="liste_objet_picture" src="<?php echo $carac_objet['img_objet']; ?>"  width="64px" height="64px"/>
+					
+		 		<div class="liste_objet_info">
+						
+					<p><span class="presentationObjetLbl">Nom:</span><span class="presentationObjetNom"><?php echo $carac_objet['nom_proprietaire']; ?> - <?php echo $carac_objet['nom_objet']; ?></span></p>
+					<p><span class="presentationObjetLbl">Categorie:</span><?php echo $carac_objet['categorie_objet']; ?></p>
+					<p><span class="presentationObjetLbl">Description:</span><?php echo $carac_objet['description_objet']; ?></p>
+							
+				</div>
+					
+			</div>
+		
+				<div class="bodycentered_line"></div>
+				<?php
+		 }
+	}
+	else{
+		?><p>Désolé, aucun objet ne correponds à votre recherche !</p><?php
+	}
 
 ?>
