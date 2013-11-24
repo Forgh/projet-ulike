cc87a758ab40903d48397f59d49993e55c98a11d
Ghostofelemantary@gmail.com <Ghostofelemantary@gmail.com@66844e33-a702-aea1-567c-434b7c635cab>
2013-11-24 20:40:54 +0000

    - Modif de connect.php

--- modeles/connect.php
+++ modeles/connect.php
@@ -9,4 +9,15 @@
 	{
 		die('Erreur : '. $e -> getMessage());
 	}
+/*
+	//BDD Free
+	try
+	{
+		$bdd = new PDO('mysql:host=projet.ulike.sql.free.fr;dbname=projet_ulike','projet.ulike','1rstcircle');
+		$bdd-> exec('SET NAMES utf8'); //On indique le jeu de caractères
+	}
+	catch (Exception $e)
+	{
+		die('Erreur : '. $e -> getMessage());
+	}*/
 ?>
