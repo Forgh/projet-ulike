81c753adf62dd7b231f6bee217001c52eaf3e008
Ghostofelemantary@gmail.com <Ghostofelemantary@gmail.com@66844e33-a702-aea1-567c-434b7c635cab>
2013-11-24 21:51:18 +0000

    - Suppression vue

--- controleurs/post_contact.php
+++ controleurs/post_contact.php
@@ -1,6 +1,6 @@
 <?php
 $to=htmlspecialchars('bob@yopmail.com');
-$sujet='Contact ULike : M/Mme '.$_POST['sujet'];
+$sujet='Contact ULike : M/Mme '.$_POST['nom'];
 $corps='Une nouvelle question vous a été posé : '.$_POST['texte'];
 $headers = 'From: ' . $_POST['mail']. '\r\n' .
 'Reply-To: prenom.nom@univ-montp2.fr' . '\r\n' .
