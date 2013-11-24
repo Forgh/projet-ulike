3478b53c2da1f3faa51e8b0bb2940db89f02b5f6
Ghostofelemantary@gmail.com <Ghostofelemantary@gmail.com@66844e33-a702-aea1-567c-434b7c635cab>
2013-11-24 13:28:51 +0000

    - Correction de bugs sur la page index.php - index.php et highlight_objet.php sont adaptés au MVC - Une ligne doit être changée sur la BDD concernant le lien de l'image du dernier objet inséré (sur l'insert du fichier .sql, il est incorrect)

--- controleurs/highlight_objet.php
+++ controleurs/highlight_objet.php
@@ -1,16 +1,16 @@
 <?php require_once('modeles/Objet.php');
 		require_once('modeles/Like.php');
+		
 	$carac_objet=Objet::getLastInsertedObjet();
-	$nom_objet = $carac_objet->getObjet();
+	//Recupération des données
+	$nom_objet = $carac_objet->getNom();
+	$categorie_objet = $carac_objet->getCategorie();
+	$description_objet = $carac_objet->getDescription();
+	$constructeur_objet = $carac_objet->getProprietaire();
+	$img_objet = $carac_objet->getImg();
+	//Récupération de TOUS les likes/dislikes
 	$number_likes= Like::getNumberOfLikes($nom_objet);
 	$number_dislikes = Like::getNumberOfDislikes($nom_objet);
+	
 ?>
-<div class="liste_objet_vote">
-	<img src="<?php echo $carac_objet['img_objet']; ?>" alt="<?php echo $carac_objet['nom_objet']; ?>"/>
-		<p class="likesNumber"><?php echo $number_likes; ?></p>
-		<p class="space"></p>
-		<p class="dislikesNumber"><?php echo $number_dislikes; ?></p><img src="css/design/dislike.png"/>
-</div>
-<p><span class="presentationObjetLbl">Nom:</span><span class="presentationObjetNom"><?php echo $carac_objet['nom_objet']; ?></span></p>
-<p><span class="presentationObjetLbl">Categorie:</span><?php echo $carac_objet['categorie_objet']; ?></p>
-<p><span class="presentationObjetLbl">Description:</span><?php echo $carac_objet['description_objet']; ?></p>
+
