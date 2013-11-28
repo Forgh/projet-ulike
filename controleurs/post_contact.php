5da072f4fd7faa1e07070658c39a97bd33a72f0b
mcflying.marty@gmail.com <mcflying.marty@gmail.com@66844e33-a702-aea1-567c-434b7c635cab>
2013-11-28 22:23:35 +0000

    Correction d'un bug dans post_contact.php.

--- controleurs/post_contact.php
+++ controleurs/post_contact.php
@@ -3,7 +3,7 @@ include("../modeles/mail.php");
 
 
 $to=htmlspecialchars('none@none.fr');
-$sujet='Contact ULike : M/Mme '.$_POST['nom'];
+$sujet='Contact ULike : M/Mme '.$_POST['sujet'];
 $corps='Une nouvelle question vous a été posé : '.$_POST['texte'];
 /*$headers = 'From: ' . $_POST['mail']. '\r\n' .
 'Reply-To: prenom.nom@univ-montp2.fr' . '\r\n' .
