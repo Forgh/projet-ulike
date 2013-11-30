fae994b0088777e25f1e69b4a1561d81c6258395
mcflying.marty@gmail.com <mcflying.marty@gmail.com@66844e33-a702-aea1-567c-434b7c635cab>
2013-11-30 21:00:26 +0000

    correction de index.php activation des mails (attention l'adresse ne peut être none@none.fr sinon affichage html) sur modeles/mail.php utilisation du css dans controleurs/activation_mail.php et controleurs/validation.php

--- controleurs/activation_mail.php
+++ controleurs/activation_mail.php
@@ -1,40 +1,61 @@
-<?php
-	include('../include/commun.php');
-	include('../modeles/connect.php');
+<?php session_start(); ?>
+<DOCTYPE html>
+<html>
 
-	include("../modeles/Entreprise.php");
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="../css/style.css" type="text/css">
+</head>
+<body>
 
-	
-	$Ent = Entreprise::getEntrepriseParGerant($_POST["login_validation"]);
-	if ($Ent != null and isset($_POST["login_validation"]) and isset($_GET["id"])){
-		$activation = sha1($GRAINE . "ENT" .  $_POST["login_validation"]);
-		if ($_GET["id"] == $activation){
-			$Ent->setConfirmed();
-			echo "L'activation a réussie.";
-		}else{
-			//PAGE code invalide (ici le login est inexistant)
-			echo "L'activation a échoué: veuillez entrer des informations valide. [ent]";
-		}
-		
-	}else{
+	<div id="bodycentered" >
+			<?php
+			include('../include/commun.php');
+			include('../modeles/connect.php');
 
-		// on suppose qu'il y a tjs moins d'entreprises que de membres
-		include("../modeles/Membre.php");
-		$Ent = Membre::getMembreParPseudo($_POST["login_validation"]); 
-		if ($Ent != null and isset($_POST["login_validation"]) and isset($_GET["id"])){
-			$activation = sha1($GRAINE . "MEM" . $_POST["login_validation"]);
-			if ($_GET["id"] == $activation){
-				$Ent->setConfirmed();
-				echo "L'activation a réussie.";
-			}else{
-				//PAGE code invalide (ici le login est inexistant)
-				echo "L'activation a échoué: veuillez entrer des informations valide.";
-			}
+			include("../modeles/Entreprise.php");
+
 			
+			$Ent = Entreprise::getEntrepriseParGerant($_POST["login_validation"]);
+			if ($Ent != null and isset($_POST["login_validation"]) and isset($_GET["id"])){
+				$activation = sha1($GRAINE . "ENT" .  $_POST["login_validation"]);
+				if ($_GET["id"] == $activation){
+					echo "<h2>Etat de la validation - Entreprise</h2>";
+					$Ent->setConfirmed();
+					echo "L'activation a réussie.";
+				}else{
+					//PAGE code invalide (ici le login est inexistant)
+					echo "<h2>Etat de la validation - Entreprise</h2>";
+					echo "L'activation a échoué: veuillez entrer des informations valide.";
+				}
+				
+			}else{
+
+				// on suppose qu'il y a tjs moins d'entreprises que de membres
+				include("../modeles/Membre.php");
+				$Ent = Membre::getMembreParPseudo($_POST["login_validation"]); 
+				if ($Ent != null and isset($_POST["login_validation"]) and isset($_GET["id"])){
+					$activation = sha1($GRAINE . "MEM" . $_POST["login_validation"]);
+					if ($_GET["id"] == $activation){
+						$Ent->setConfirmed();
+						echo "<h2>Etat de la validation - Membre</h2>";
+						echo "L'activation a réussie.";
+					}else{
+						//PAGE code invalide (ici le login est inexistant)
+						echo "<h2>Etat de la validation - Membre</h2>";
+						echo "L'activation a échoué: veuillez entrer des informations valide.";
+					}
+					
+				
+				}else{
+					//PAGE activation echoue (ici c'est possible que le login soit inexistant)
+					echo "<h2>Erreur</h2>";
+					echo "L'activation a échoué: veuillez entrer des informations valide.";
+				}
+			}
+			?>
+	</div>
 		
-		}else{
-			//PAGE activation echoue (ici c'est possible que le login soit inexistant)
-			echo "L'activation a échoué: veuillez entrer des informations valide.²";
-		}
-	}
-?>
+
+</body>
+</html>
